a4d7ac0c74fc07aaeb92d25a41db3782396bb99a
Gianni Nitti <gnitti@collaboration.codearchitects.net>
2016-06-09 15:14:14 +0200

    FirstCommit

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace StringCalculatorTests
+{
+  public class Calculator
+  {
+    
+    public int Add(string parameter)
+    {
+      int retval = default(int);
+      if (string.IsNullOrEmpty(parameter))
+      {
+        retval = 0;
+      }
+      else
+      {
+        var listOfNUmber = parameter.Split(",".ToCharArray());
+        if (listOfNUmber.Length > 0)
+        {
+          foreach (var item in listOfNUmber)
+          {
+            retval = retval + int.Parse(item);
+          }
+        }
+        else
+        {
+          int.TryParse(parameter, out retval);
+        }
+      }
+      return retval;
+    }
+  }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalculatorTests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void StringaVuotaDeveRestituireZero()
+    {
+      Calculator sut = new Calculator();
+
+      var actual = sut.Add("");
+      Assert.AreEqual(0, actual);
+    }
+
+    [TestMethod]
+    public void StringaConUnNumeroDeveRestituireSeStesso()
+    {
+      Calculator sut = new Calculator();
+      var parameter = "1";
+      var actual = sut.Add(parameter);
+      Assert.AreEqual(1, actual);
+    }
+
+    [TestMethod]
+    public void StringaConDueNumeriDeveRestituireLaSomma()
+    {
+      Calculator sut = new Calculator();
+      var parameter = "1,5";
+      var actual = sut.Add(parameter);
+      Assert.AreEqual(6, actual);
+    }
+
+  }
+}
